00fcde87d710b055534f24042476e47f88da73e3
Joseph Keiyuru <josephkeiyuru1@gmail.com>
2024-10-19 17:56:03 +0300

    Completed the JS code -thus completing the tutorial.

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ function createBoard(containerId, initialState = null) {
     if (initialState) {
         updateBoard(containerId, initialState);
     }
+}
+
+    // This function will update the board state so that wherever the disc is placed, it will be displayed
     function updateBoard(containerId, state) {
         const container = document.getElementById(containerId);
         const cells = container.querySelectorAll('.cell');
@@ -46,3 +49,99 @@ function createBoard(containerId, initialState = null) {
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     ];
+
+
+    // Sample game states for the game tutorial.
+const sampleGameStates = [
+    // Initial state (We call the initial state array (board))
+    initialState,
+    // Step 1: Black places a disc
+    
+    [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'W', 'B', 'B', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ], 
+    // Step 2: White places a disc
+    [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'W', 'W', 'B', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ],
+    // Step 3: Black places another disc
+    [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', 'B', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ],
+    // Step 4: White places a disc
+    [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', 'B', ' ', ' '],
+        [' ', ' ', 'W', 'W', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ],
+
+    // step 5: Black places another disc
+    [
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', 'B', 'B', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', 'B', 'W', 'B', ' ', ' '],
+        [' ', ' ', 'W', 'W', 'W', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    ]
+
+    // We can add more moves as need arises
+];
+
+//HERE
+
+let currentStep = 0;
+
+// This function will be for initializing the game
+function initGame() {
+    createBoard('initial-board', initialState);
+    createBoard('sample-board', sampleGameStates[0]);
+
+    const prevButton = document.getElementById('prev-move');
+    const nextButton = document.getElementById('next-move');
+
+    prevButton.addEventListener('click', () => {
+        if (currentStep > 0) {
+            currentStep--;
+            updateBoard('sample-board', sampleGameStates[currentStep]);
+        }
+    });
+
+    nextButton.addEventListener('click', () => {
+        if (currentStep < sampleGameStates.length - 1) {
+            currentStep++;
+            updateBoard('sample-board', sampleGameStates[currentStep]);
+        }
+    });
+}
+
+// Initialize the game when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', initGame);
